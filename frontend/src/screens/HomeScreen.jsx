f6f12d3bd4ff3aec4ad1f8c5bff33ba695d982ec
MadhuriTeli <madhuriteli07@gmail.com>
2024-07-14 00:52:58 +0530

    Loader & Message components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice.js';
+import Loader from '../components/Loader.jsx';
+import Message from '../components/Message.jsx';
 
 const HomeScreen = () => {
     const { data: products, isLoading, error } = useGetProductsQuery();
     return (
         <>
             {isLoading ? (
-                <h2>Loading...</h2>
-            ) : error ? (<div>{error?.data?.message || error.error}</div>) : (<>
+                <Loader />
+            ) : error ? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) : (<>
                 <h1>Latest Products</h1>
                 <Row>
                     {products.map((product) => (
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,81 +3,92 @@ import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice.js";
+import Loader from "../components/Loader.jsx";
+import Message from "../components/Message.jsx";
 
 const ProductScreen = () => {
-  const {id:productId} = useParams();
-  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);  
+  const { id: productId } = useParams();
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <Row>
+            <Col md={5}>
+              <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={4}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>{product.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                <ListGroup.Item>
+                  Description: ${product.description}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
 
-{isLoading ? (
-  <h2>Loading...</h2>
-) : error ? (<div>{error?.data?.message || error.error}</div>) : (
-<>
-<Row>
-<Col md={5}>
-  <Image src={product.image} alt={product.name} fluid />
-</Col>
-<Col md={4}>
-  <ListGroup variant="flush">
-    <ListGroup.Item>
-      <h3>{product.name}</h3>
-    </ListGroup.Item>
-    <ListGroup.Item>
-      <Rating
-        value={product.rating}
-        text={`${product.numReviews} reviews`}
-      />
-    </ListGroup.Item>
-    <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-    <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
-  </ListGroup>
-</Col>
-<Col md={3}>
-  <Card>
-    <ListGroup variant="flush">
-      <ListGroup.Item>
-        <Row>
-          <Col>Price:</Col>
-          <Col>
-            <strong>${product.price}</strong>
-          </Col>
-        </Row>
-      </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        <strong>
+                          $
+                          {product.countInStock > 0
+                            ? "In Stock"
+                            : "Out Of Stock"}
+                        </strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
 
-      <ListGroup.Item>
-        <Row>
-          <Col>Status:</Col>
-          <Col>
-            <strong>
-              ${product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-            </strong>
-          </Col>
-        </Row>
-      </ListGroup.Item>
-
-      <ListGroup.Item>
-        <Button
-          className="btn-block"
-          type="button"
-          disabled={product.countInStock === 0}
-        >
-          Add To Cart
-        </Button>
-      </ListGroup.Item>
-    </ListGroup>
-  </Card>
-</Col>
-</Row>
-</>
-
-) }
-
-     
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block"
+                      type="button"
+                      disabled={product.countInStock === 0}
+                    >
+                      Add To Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </>
+      )}
     </>
   );
 };
