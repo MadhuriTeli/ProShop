3c446c8e760ac8f35ead3997b594b8b5a1fc0157
MadhuriTeli <madhuriteli07@gmail.com>
2024-07-13 21:17:06 +0530

    Custom Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,7 +15,8 @@ router.get("/:id", asyncHandler(async(req, res) => {
     if(product){  
         return res.json(product);
     }
-    res.status(404).json({message:"Product not found"});
+    res.status(404);
+    throw new Error ("Resouce not found");
 }));
 
 export default router;
