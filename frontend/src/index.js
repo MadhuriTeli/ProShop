13d3c61285ed3af70cca0596da7f73e1f9b9a266
MadhuriTeli <madhuriteli07@gmail.com>
2024-08-07 01:17:07 +0530

    cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import store from './store';
 import {Provider} from 'react-redux';
+import CartSceen from './screens/CartSceen';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const router = createBrowserRouter(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartSceen />} />
     </Route>
   )
 );
