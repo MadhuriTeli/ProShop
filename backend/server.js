725aef6057c9887f6e5c002048b1584898c62558
MadhuriTeli <madhuriteli07@gmail.com>
2024-08-07 17:49:07 +0530

    User email  & password validation

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -9,6 +9,12 @@ const port = process.env.PORT || 5000;
 
 connectDB();    //Connect to MongoDB
 const app = express();
+
+//middleware
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 
 app.get("/", (req, res) => {
     res.send("API is running...");
