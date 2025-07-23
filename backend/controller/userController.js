d034566efba59fc603c76c7ff56fda9e2ccd009a
MadhuriTeli <madhuriteli07@gmail.com>
2025-07-23 18:00:08 +0530

    User registration password encryption

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -25,13 +25,40 @@ const authUser = asyncHandler(async(req, res)=>{
     }
 });
 
-// @desc User auth
-// @route POST /api/users
-// @access Public
-const registerUser = asyncHandler(async(req, res)=>{
-   
-    res.send('register user');
-});
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+  
+    const userExists = await User.findOne({ email });
+  
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+  
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+  
+    if (user) {
+      generateToken(res, user._id);
+  
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  });
+  
 
 // @desc logout user/ clear cookies
 // @route POST /api/users/logout
@@ -41,12 +68,24 @@ const logoutUser = asyncHandler(async(req, res)=>{
 });
 
 
-// @desc  get user profile
-// @route GET /api/users/profile
-// @access Private
-const getUserProfile = asyncHandler(async(req, res)=>{
-    res.send('get user profile');
-});
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+  
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
+    }
+  });
 
 // @desc update user profile
 // @route PUT /api/users/profile
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {authUser, registerUser, logoutUser, getUserProfile , updateUserProfile , getUsers, getUserById, deleteUser, updateUser} from '../controller/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').post(registerUser).get(getUsers);
 router.post('/logout', logoutUser);
 router.post("/auth", authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(updateUserProfile);
 router.route('/:id').delete(deleteUser).get(getUserById).put(updateUserProfile);
 
 
