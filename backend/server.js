5494b7acec9d13c64922d1886f70e141fa576766
MadhuriTeli <madhuriteli07@gmail.com>
2025-07-23 12:56:04 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,12 @@ import connectDB from './config/db.js';
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from './routes/userRouter.js';
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+import cors from "cors";
 
 connectDB();    //Connect to MongoDB
 const app = express();
-
+app.use(cors());
 //middleware
 
 app.use(express.json());
@@ -18,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
     res.send("API is running...");
-    console.log(products);
+    // console.log(products);
 });
 
 
@@ -29,6 +30,5 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => {
-//  console.log(products);
   console.log(`Server running on port ${port}`)
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+Seed Data into MongoDB
+//node backend/seeder.js
+
+run the server
+//nodemon backend/server.js
